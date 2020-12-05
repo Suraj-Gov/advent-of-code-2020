dbcd9618e674859e51ebfc5b753fdf353b76b04e
Suraj Govind <surajgovind332000@gmail.com>
2020-12-05 18:15:42 +0530

    completed day 4 part 2

--- day4/day4.js
+++ day4/day4.js
@@ -1,4 +1,5 @@
 var fs = require("fs");
+const { cpuUsage } = require("process");
 
 const input = fs
   .readFileSync("./input.txt", {
@@ -19,10 +20,11 @@ const input = fs
 // hgt:179cm
 
 // hcl:#cfa07d eyr:2025 pid:166559648
-// iyr:2011 ecl:brn hgt:59in`.split("\n\n");
+// iyr:2011 ecl:brn hgt:59in`.split("\r\n\r\n");
 
 const cleanInput = (input) => {
-  return input.split("\n").join(" ").slice(0, -1).split(" ");
+  const cleaned = input.split("\r\n").join(" ").split(" ");
+  return cleaned;
 };
 
 const validateInput = (input) => {
@@ -43,9 +45,42 @@ const validAttr = (inputObjAttr, limit1, limit2) => {
   );
 };
 
-const validHeight = (height = {
-  // todo
-});
+const validHeight = (height) => {
+  const unit = height.slice(-2);
+  const val = parseInt(height.slice(0, -2));
+  if (unit === "cm") {
+    return val >= 150 && val <= 193;
+  } else if (unit === "in") {
+    return val >= 59 && val <= 76;
+  } else return false;
+};
+
+const validHair = (hair) => {
+  if (hair[0] === "#") {
+    const regex = /[0-9a-f]/g;
+    if (regex.exec(hair.slice(1).length === 6)) {
+      return true;
+    } else return false;
+  } else return false;
+};
+
+const validEye = (eye) => {
+  const validEyes = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+  return validEyes.some((i) => i === eye);
+};
+
+const validPassport = (passport) => {
+  if (passport.length === 9) {
+    const len = passport
+      .split("")
+      .reduce(
+        (total, curr) => (/[0-9]/gi.exec(curr).length > 0 ? total + 1 : total),
+        0
+      );
+    // len.forEach((i) => console.log(/[0-9]/gi.exec(i)));
+    return len === 9;
+  } else return false;
+};
 
 const isValidPassport = (inputObj) => {
   if (
@@ -55,6 +90,19 @@ const isValidPassport = (inputObj) => {
     const validByr = validAttr(inputObj.byr, 1920, 2002);
     const validIyr = validAttr(inputObj.iyr, 2010, 2020);
     const validEyr = validAttr(inputObj.eyr, 2020, 2030);
+    const validHgt = validHeight(inputObj.hgt);
+    const validHcl = validHair(inputObj.hcl);
+    const validEcl = validEye(inputObj.ecl);
+    const validPid = validPassport(inputObj.pid);
+    return (
+      validByr &&
+      validIyr &&
+      validEyr &&
+      validHgt &&
+      validHcl &&
+      validEcl &&
+      validPid
+    );
   }
 };
 
