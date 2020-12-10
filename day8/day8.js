a5ccf2badb6491c94532f68b9541fbcdf8bb19b5
Suraj-Gov <surajgovind332000@gmail.com>
2020-12-10 08:18:10 +0530

    fix day 8 part 2, not werking 😐

--- day8/day8.js
+++ day8/day8.js
@@ -1,13 +1,13 @@
 var fs = require("fs");
 
 const input = fs
-  .readFileSync("./input.txt", {
+  .readFileSync("./test.txt", {
     encoding: "utf-8",
     flag: "r",
   })
   .split("\r\n");
 
-const inputExec = input.map((_) => 0);
+let inputExec = input.map((_) => 0);
 
 let pointer = 0;
 let acc = 0;
@@ -16,23 +16,56 @@ const readInput = (input) => {
   return input.split(" ");
 };
 
-while (true) {
-  if (inputExec[pointer] === 0) {
-    inputExec[pointer] = 1;
-  } else break;
-  const instruction = readInput(input[pointer]);
-  switch (instruction[0]) {
-    case "nop":
-      pointer += 1;
+const findAccValPart1 = (input) => {
+  while (true) {
+    if (inputExec[pointer] === 0) {
+      inputExec[pointer] = 1;
+    } else {
+      console.log(acc, "noo");
+      acc = 0;
+      pointer = 0;
+      inputExec = input.map((_) => 0);
+      return false;
+    }
+    // const instruction = readInput(input[pointer]);
+    let instruction = input[pointer];
+    switch (instruction[0]) {
+      case "nop":
+        pointer += 1;
+        break;
+      case "acc":
+        acc += parseInt(instruction[1]);
+        pointer += 1;
+        if (pointer === input.length) {
+          console.log(acc, "yes");
+          acc = 0;
+          pointer = 0;
+          inputExec = input.map((_) => 0);
+          return false;
+        }
+        break;
+      case "jmp":
+        pointer += parseInt(instruction[1]);
+        break;
+    }
+  }
+};
+
+const origIns = input.map((i) => readInput(i));
+let alterIns = origIns;
+
+for (let i = 0; i < origIns.length; i++) {
+  if (alterIns[i][0] === "jmp") {
+    alterIns[i][0] = "nop";
+    if (findAccValPart1(alterIns) === true) {
       break;
-    case "acc":
-      acc += parseInt(instruction[1]);
-      pointer += 1;
+    }
+    alterIns[i][0] = "jmp";
+  } else if (alterIns[i][0] === "nop") {
+    alterIns[i][0] = "jmp";
+    if (findAccValPart1(alterIns) === true) {
       break;
-    case "jmp":
-      pointer += parseInt(instruction[1]);
-      break;
+    }
+    alterIns[i][0] = "nop";
   }
 }
-
-console.log(acc);
